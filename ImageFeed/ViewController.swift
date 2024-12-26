9bb9bacae6ac30385ac8cd9c2f2d30f0d69d060f
artemiytolkisevskiy <evgrafiyha@gmail.com>
2024-12-26 17:36:56 +0300

    Initial Commit

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Артемий Толкишевский on 26.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
